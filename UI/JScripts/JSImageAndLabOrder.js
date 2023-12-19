7027334c4b584667fa927daa342c9cb76e2cfec8
Nitin MS <Nitin.Ms@india.akidolabs.com>
2023-12-19 18:04:56 +0530

    CAP-1498

--- UI/JScripts/JSImageAndLabOrder.js
+++ UI/JScripts/JSImageAndLabOrder.js
@@ -107,6 +107,8 @@ function OpenPDFImage(FaxSubject) {
     $(top.window.document).find("#PrintPDFmdldlg")[0].style.height = "750px";
     $(top.window.document).find("#PrintPDFFrame")[0].style.height = "685px";
     $(top.window.document).find("#PrintPDFFrame")[0].contentDocument.location.href = "frmPrintPDF.aspx?SI=" + document.getElementById('hdnSelectedItem').value + "&Location=DYNAMIC" + "&FromOrder=Y";
+    //CAP-1498
+    document.getElementById('btnOrderSubmit').disabled = true;
 }
 function OnClientCloseDiagnosis(oWindow, args) {
     if (oWindow != null) {
