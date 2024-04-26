1706d8fd09b1899b5cdeff71170373a7249a0901
Anurag Singh <Anurag.Singh@india.akidolabs.com>
2024-04-26 00:44:22 -0700

    CAP-1955: fix compile time error

--- UI/frmLandingScreen.aspx.cs
+++ UI/frmLandingScreen.aspx.cs
@@ -143,7 +143,6 @@ namespace Acurus.Capella.UI
                     ExpireRedirectUrlCookie();
                     }
                 }
-            }
 
             if (string.IsNullOrWhiteSpace(sUserAccountType))
             {
