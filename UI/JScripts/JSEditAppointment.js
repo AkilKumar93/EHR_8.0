33ee356e63f0b0c5a40b5b15f4da9cb11da4b0a9
Deepak Tamilmani <Deepak.Tamilmani@india.akidolabs.com>
2024-07-18 05:50:48 -0700

    Edit button enable change

--- UI/JScripts/JSEditAppointment.js
+++ UI/JScripts/JSEditAppointment.js
@@ -1688,9 +1688,11 @@ function EnableProviderSearch(EnablePrvdr)
     else if (EnablePrvdr == 'true'){
         if (document.getElementById("txtProviderSearch").value == "") {
             document.getElementById("txtProviderSearch").disabled = false;
+            document.getElementById("imgEditProvider").style.display = "block";
         }
         else {
             document.getElementById("txtProviderSearch").disabled = true;
+            document.getElementById("imgEditProvider").style.display = "none";
         }
         if (document.getElementById("imgClearProviderText")?.style?.visibility != undefined && document.getElementById("imgClearProviderText")?.style?.visibility != null) {
             document.getElementById("imgClearProviderText").style.visibility = true;
