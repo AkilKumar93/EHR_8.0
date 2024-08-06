b4dcfddb3c3ba2d91f89785756238c15e40af094
Deepak Tamilmani <Deepak.Tamilmani@india.akidolabs.com>
2024-08-05 21:46:59 -0700

    jira CAP-2244

--- UI/JScripts/JsRCopiaMergePatientBar.js
+++ UI/JScripts/JsRCopiaMergePatientBar.js
@@ -1,12 +1,13 @@
 ﻿function OnTabClick(e)
 {
+    var human_id = document.URL.slice(document.URL.indexOf("HumanID")).split("&")[0].split("=")[1];
     if (event?.currentTarget?.id != undefined && event?.currentTarget?.id != null) {
         if (event?.currentTarget?.id == "btnMedication") {
             $("#btnAllergies").removeClass("btncolorMyQ");
             $("#btnMedication").addClass("btncolorMyQ");
             $("#lblScreenDis")[0].innerText = "List of Duplicate Medications in Keep Account";
             $("#ifrmRcopiaDuplicateScreen")[0].src = "";
-            $("#ifrmRcopiaDuplicateScreen")[0].src = "frmRCopiaDuplicateMediations.aspx";
+            $("#ifrmRcopiaDuplicateScreen")[0].src = "frmRCopiaDuplicateMediations.aspx?HumanID=" + human_id;
         }
         else if (event?.currentTarget?.id == "btnAllergies") {
             $("#btnMedication").removeClass("btncolorMyQ");
