10719e97ef5795658d197a0c4a64444b9662f08d
Deepak Tamilmani <Deepak.Tamilmani@india.akidolabs.com>
2025-01-21 01:24:04 -0800

    CAP-2791 and CAP-2792 - Give timeout to set the height of Queue tables.

--- UI/JScripts/JSMyQueueNew.js
+++ UI/JScripts/JSMyQueueNew.js
@@ -5775,12 +5775,14 @@ function Decompress(data) {
 }
 
 function SetHeightForTabelBasedOnScreenSize() {
-    var MaxHeightTable = (document.getElementById("launcher").getBoundingClientRect().top - document.getElementsByClassName("dataTables_scrollBody")[0].getBoundingClientRect().top)
-        + (document.getElementById("launcher").getBoundingClientRect().height - 10);
-    if (MaxHeightTable != undefined) {
-        if ($(".dataTables_scrollBody")?.css("max-height") != undefined
-            && parseInt($(".dataTables_scrollBody").css("max-height")) < MaxHeightTable) {
-            $(".dataTables_scrollBody").css({ "max-height": MaxHeightTable + "px" });
+    window.setTimeout(function () {
+        var MaxHeightTable = (document.getElementById("launcher").getBoundingClientRect().top - document.getElementsByClassName("dataTables_scrollBody")[0].getBoundingClientRect().top)
+            + (document.getElementById("launcher").getBoundingClientRect().height - 10);
+        if (MaxHeightTable != undefined) {
+            if ($(".dataTables_scrollBody")?.css("max-height") != undefined
+                && parseInt($(".dataTables_scrollBody").css("max-height")) < MaxHeightTable) {
+                $(".dataTables_scrollBody").css({ "max-height": MaxHeightTable + "px" });
+            }
         }
-    }
+    }, 500);
 }
