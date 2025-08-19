123febb991e4823cedfcd55a0dcb0d2ab4cd4c85
Deepak Tamilmani <Deepak.Tamilmani@india.akidolabs.com>
2025-08-19 03:17:33 -0700

    Jira CAP-3559 - get the cookies variable properly

--- UI/JScripts/JsLogRocket.js
+++ UI/JScripts/JsLogRocket.js
@@ -3,14 +3,13 @@ if (sIsEnableLogRocket == "Y") {
     const script = document.createElement('script');
     script.src = 'https://cdn.lgrckt-in.com/LogRocket.min.js';
     script.onload = () => {
-        document.head.appendChild(script);
 
         var sUsername = (document?.cookie?.slice(document?.cookie?.indexOf("CUserName"))?.split(";")[0]?.split("=")[1]) ?? "";
         var sPersonname = (document?.cookie?.slice(document?.cookie?.indexOf("CPersonName"))?.split(";")[0]?.split("=")[1]) ?? "";
         var sEMailAddress = (document?.cookie?.slice(document?.cookie?.indexOf("CEMailAddress"))?.split(";")[0]?.split("=")[1]) ?? "";
         var sLogRocketClientID = (document?.cookie?.slice(document?.cookie?.indexOf("CLogRocketClientID"))?.split(";")[0]?.split("=")[1]) ?? "";
         var sVersion = (document?.cookie?.slice(document?.cookie?.indexOf("CVersion"))?.split(";")[0]?.split("=")[1]) ?? "";
-
+        
         LogRocket.init(sLogRocketClientID,
             {
                 mergeIframes: true,
