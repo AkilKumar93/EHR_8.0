ec8eb5af2a6b17f5d62d6870276d7a429fbdca99
Nitin MS <Nitin.Ms@india.akidolabs.com>
2025-08-26 03:57:24 -0700

    CAP-3293 - Left Side Patient Chart - Akido Patient Profile

---
+++ DataAccess/ManagerObjects/ApplicationUsageLogManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Acurus.Capella.Core.DomainObjects;
+
+namespace Acurus.Capella.DataAccess.ManagerObjects
+{
+    public partial interface IApplicationUsageLogManager : IManagerBase<ApplicationUsageLog, ulong>
+    {
+        void SaveRcopia_deduplicate_logWithTransaction(IList<ApplicationUsageLog> ilstApplication_Usage_Log, IList<ApplicationUsageLog> ListToUpdateApplication_Usage_Log, string MACAddress);
+    }
+
+    public partial class ApplicationUsageLogManager : ManagerBase<ApplicationUsageLog, ulong>, IApplicationUsageLogManager
+    {
+        #region Constructors
+
+        public ApplicationUsageLogManager()
+            : base()
+        {
+
+        }
+        public ApplicationUsageLogManager
+            (INHibernateSession session)
+            : base(session)
+        {
+
+        }
+        #endregion
+
+        #region Methods
+
+        public void SaveRcopia_deduplicate_logWithTransaction(IList<ApplicationUsageLog> ilstApplication_Usage_Log, IList<ApplicationUsageLog> ListToUpdateApplication_Usage_Log, string MACAddress)
+        {
+            SaveUpdateDeleteWithTransaction(ref ilstApplication_Usage_Log, ListToUpdateApplication_Usage_Log, null, MACAddress);
+        }
+        #endregion
+    }
+}
---
+++ UI/WebServices/LogService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="LogService.asmx.cs" Class="Acurus.Capella.UI.WebServices.LogService" %>
